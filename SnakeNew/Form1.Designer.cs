b921fa9ebc1a4c5a308ead8e9672995e98ae3f7e
José Manuel Guilherme Alexandre <josealex1969@gmail.com>
2022-02-28 11:52:05 +0000

    Add project files.

---
+++ SnakeNew/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace SnakeNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.snake = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.snake)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snake
+            // 
+            this.snake.BackColor = System.Drawing.Color.Red;
+            this.snake.Location = new System.Drawing.Point(430, 170);
+            this.snake.Name = "snake";
+            this.snake.Size = new System.Drawing.Size(13, 12);
+            this.snake.TabIndex = 1;
+            this.snake.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pontos:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.snake);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.snake)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox snake;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SnakeNew/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeNew
+{
+    public partial class Form1 : Form
+    {
+        bool direita, esquerda, cima, baixo;
+        PictureBox[] lsnake = new PictureBox[50];
+        PictureBox comida = new PictureBox();
+        int ncorpo = 0;
+        int pontos = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lsnake[0] = snake;
+            Comida();
+            snake.Location = new Point(200,300);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    cima = true; baixo = false; direita = false; esquerda = false;
+                    break;
+                case Keys.Down:
+                    cima = false; baixo = true; direita = false; esquerda = false;
+                    break;
+                case Keys.Left:
+                    cima = false; baixo = false; direita = false; esquerda = true;
+                    break;
+                case Keys.Right:
+                    cima = false; baixo = false; direita = true; esquerda = false;
+                    break;
+            }
+        }
+        void Comida()
+        {
+            Random rand = new Random();
+            comida.BackColor = Color.Green;
+            comida.Size = new Size(10,10);
+            comida.Location = new Point(rand.Next(1, 80) * 10, rand.Next(1, 60) * 10);
+            this.Controls.Add(comida);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = ncorpo; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    if (cima)
+                        lsnake[0].Location = new Point(lsnake[0].Location.X, lsnake[0].Location.Y - 10);
+                    if (baixo)
+                        lsnake[0].Location = new Point(lsnake[0].Location.X, lsnake[0].Location.Y + 10);
+                    if (esquerda)
+                        lsnake[0].Location = new Point(lsnake[0].Location.X - 10, lsnake[0].Location.Y);
+                    if (direita)
+                        lsnake[0].Location = new Point(lsnake[0].Location.X + 10, lsnake[0].Location.Y);
+
+                }
+                else if (ncorpo > 0)
+                {
+                    lsnake[i].Location = new Point(lsnake[i - 1].Location.X, lsnake[i - 1].Location.Y);
+                }
+            }
+            if (comida.Location.X == snake.Location.X && comida.Location.Y == snake.Location.Y)
+            {
+                Comida();
+                ncorpo += 1;
+                PictureBox corpo = new PictureBox();
+                lsnake[ncorpo] = corpo;
+                corpo.Location = new Point(lsnake[ncorpo].Location.X, lsnake[ncorpo].Location.Y);
+                corpo.Width = 10;
+                corpo.Height = 10;
+                corpo.BackColor = Color.Blue;
+                this.Controls.Add(corpo);
+                pontos += 10;
+                label1.Text = "Pontos: " + pontos;
+            }
+        }
+    }
+}
